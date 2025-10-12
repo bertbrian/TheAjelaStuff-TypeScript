6d1497989fb4fe1afac6ff7d55b686171c17da4a
bertbrian <bertbrian97@gmail.com>
2025-10-13 02:38:16 +0700

    authentication with next auth

--- auth.ts
+++ auth.ts
@@ -43,6 +43,7 @@ export const config = {
             return {
               id: user.id,
               name: user.name,
+              email: user.email,
               role: user.role,
             };
           }
@@ -56,11 +57,31 @@ export const config = {
     async session({ session, user, trigger, token }: any) {
       // Set the user ID from the token
       session.user.id = token.sub;
+      session.user.role = token.role;
+      session.user.name = token.name;
 
       // If there is an update, set the user name
       if (trigger === "update") session.user.name = user.name;
 
       return session;
+    },
+    async jwt({ token, user, trigger, session }: any) {
+      // Assign user field to token
+      if (user) {
+        token.role = user.role;
+
+        // IF user has no name then use the email
+        if (user.name === "NO_NAME") {
+          token.name = user.email!.split("@")[0];
+
+          // Update database to reflect the token name
+          await prisma.user.update({
+            where: { id: user.id },
+            data: { name: token.name },
+          });
+        }
+      }
+      return token;
     },
   },
 } satisfies NextAuthConfig;
