6248f7889737ced5a48b08f0628b0d08dfed35f4
bertbrian <bertbrian97@gmail.com>
2025-11-13 22:28:28 +0700

    fixing error, remove unused variable

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -14,7 +14,7 @@ const NotFoundPage = () => {
         priority={true}
       />
       <div className="p-6 rounded-lg shadow-md text-center w-1/3">
-        <h1 className="text-3xl fontbold mb-4">Not Found</h1>
+        <h1 className="text-3xl font-bold mb-4">Not Found</h1>
         <p className="text-destructive">Could not find requested page</p>
         <Button
           variant="outline"
